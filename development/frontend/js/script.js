0d1ee6cbbf00e608258d132de64b08285042aeae
Clifford Paulick <tko@tourkick.com>
2020-01-12 14:05:30 -0600

    Reformat code and remove hard-coded styling

--- development/frontend/js/script.js
+++ development/frontend/js/script.js
@@ -1,39 +1,39 @@
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in the file "development/admin/js/script.js".
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared (import $ from 'jquery')for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 *
-	 * The file is enqueued from src/admin/class-assets.php.
-	 */
-	import $ from 'jquery';
-	// @TODO This is an example import. Remove for production
-    import './components/test';
-    
-    $(document).ready(function() {
-        // jQuery methods go here...
-    });
-    // @TODO This is an example console.log(). Remove for production
-    console.log("frontend_script.js");
-    
+/**
+ * All of the code for your admin-facing JavaScript source
+ * should reside in the file "development/admin/js/script.js".
+ *
+ * Note: It has been assumed you will write jQuery code here, so the
+ * $ function reference has been prepared (import $ from 'jquery')for usage within the scope
+ * of this function.
+ *
+ * This enables you to define handlers, for when the DOM is ready:
+ *
+ * $(function() {
+ *
+ * });
+ *
+ * When the window is loaded:
+ *
+ * $( window ).load(function() {
+ *
+ * });
+ *
+ * ...and/or other possibilities.
+ *
+ * Ideally, it is not considered best practise to attach more than a
+ * single DOM-ready or window-load handler for a particular page.
+ * Although scripts in the WordPress core, Plugins and Themes may be
+ * practising this, we should strive to set a better example in our own work.
+ *
+ * The file is enqueued from src/admin/class-assets.php.
+ */
+import $ from 'jquery';
+// @TODO This is an example import. Remove for production
+import './components/test';
+
+$( document ).ready( function () {
+	// jQuery methods go here...
+} );
+
+// @TODO This is an example console.log(). Remove for production
+console.log( 'frontend_script.js' );
