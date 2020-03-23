878c936a0e4104c72ecde550286508319ee4ff9a
Clifford Paulick <tko@tourkick.com>
2020-03-23 01:52:01 -0500

    admin color scheme primaries

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,14 @@ module.exports = {
 		'postcss-advanced-variables': {
 			'variables': {
 				'plugin-slug': 'cliff-wp-plugin-boilerplate',
+				'admin-color-default-primary': '#00a0d2',
+				'admin-color-light-primary': '#04a4cc',
+				'admin-color-blue-primary': '#52accc',
+				'admin-color-coffee-primary': '#59524c',
+				'admin-color-ectoplasm-primary': '#523f6d',
+				'admin-color-midnight-primary': '#363b3f',
+				'admin-color-ocean-primary': '#738e96',
+				'admin-color-sunrise-primary': '#cf4944',
 			},
 		},
 	},
